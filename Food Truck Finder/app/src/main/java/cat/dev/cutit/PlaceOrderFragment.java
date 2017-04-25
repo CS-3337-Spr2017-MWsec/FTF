c0688313b7dde73d828962d1db29165cc080753b
Karina Martinez <karinalmartinez93@gmail.com>
2017-04-24 19:02:25 -0700

    Modify Place Order Fragment

--- Food Truck Finder/app/src/main/java/cat/dev/cutit/PlaceOrderFragment.java
+++ Food Truck Finder/app/src/main/java/cat/dev/cutit/PlaceOrderFragment.java
@@ -6,9 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 
 public class PlaceOrderFragment extends Fragment {
+
+    private static String TAG = "PlaceOrderFragment";
+    private int MAX_ORDER_ITEMS = 20;
+    private int totalOrderItems = 0;
+    private EditText mOrderItems[]=new EditText[MAX_ORDER_ITEMS];
+    private EditText mOrderItemsDescription[] = new EditText[MAX_ORDER_ITEMS];
+    private EditText mOrderItemsQuantity[] = new EditText[MAX_ORDER_ITEMS];
+    private View mRegisterForm;
+    private View mProgressView;
+
+
+
+    private FirebaseAuth mAuthentication;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
