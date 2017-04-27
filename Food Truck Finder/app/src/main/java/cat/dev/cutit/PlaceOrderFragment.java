a36398a5f49baaae08972d230b15d5c3a47b726e
Karina Martinez <karinalmartinez93@gmail.com>
2017-04-26 19:06:48 -0700

    Change PlaceOrderFragment.java

--- Food Truck Finder/app/src/main/java/cat/dev/cutit/PlaceOrderFragment.java
+++ Food Truck Finder/app/src/main/java/cat/dev/cutit/PlaceOrderFragment.java
@@ -1,19 +1,16 @@
 package cat.dev.cutit;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
@@ -21,13 +18,11 @@ import java.util.ArrayList;
 
 public class PlaceOrderFragment extends Fragment {
 
-    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
     private static String TAG = "PlaceOrderFragment";
     private int MAX_ORDER_ITEMS = 20;
     private int totalOrderItems = 0;
-    private EditText mOrderItems[]=new EditText[MAX_ORDER_ITEMS];
-    private EditText mOrderItemsDescription[] = new EditText[MAX_ORDER_ITEMS];
-    private EditText mOrderItemsQuantity[] = new EditText[MAX_ORDER_ITEMS];
     private View mRegisterForm;
     private View mProgressView;
     private String orderKey = ref.push().getKey();
@@ -45,5 +40,20 @@ public class PlaceOrderFragment extends Fragment {
 
 
     }
+    public void getOrder(){
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Order order = dataSnapshot.getValue(Order.class);
+                System.out.println(order);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
 
 }
