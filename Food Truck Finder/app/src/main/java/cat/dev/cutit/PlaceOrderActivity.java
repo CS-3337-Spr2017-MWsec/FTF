1cad9ab2628e5960e977d287368b633478b81252
Karina Martinez <karinalmartinez93@gmail.com>
2017-05-01 11:08:56 -0700

    Add place order activity

---
+++ Food Truck Finder/app/src/main/java/cat/dev/cutit/PlaceOrderActivity.java
@@ -0,0 +1,13 @@
+package cat.dev.cutit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaceOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_order);
+    }
+}
